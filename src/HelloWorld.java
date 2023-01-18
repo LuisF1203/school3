03c922e1af2c42460a331ed6fc93ebfe987e5039
LuisF <luisfer.montesblanco@Outlook.com>
2023-01-18 09:30:36 -0600

    first

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/*
+
+Esto es la clase principal
+*/
+public class HelloWorld{
+    /**
+     * @param args almacena los argumentosdel programa
+     *             este es el metodo de arranque de toda aplicaion java
+     */
+    public static void main (String[] args){
+        System.out.println("hola mundo");
+    }
+}
